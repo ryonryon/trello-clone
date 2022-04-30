0a5bcb26ac262de1827033bac305b78e46d23b83
kei95 <kei0613shine.star@gmail.com>
2022-04-30 16:43:42 -0700

    Tweat button's story to adopt args as POC

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,29 +1,39 @@
 import styled from "styled-components";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
 import { Add } from "@material-ui/icons";
 import Button from "./Button";
 
 export default {
   title: "Button",
   component: Button,
+  argTypes: { onClick: { action: "clicked. Args" } },
+  args: {
+    title: "Love!",
+  },
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => (
+  <BackGround>
+    <Button {...args} />
+  </BackGround>
+);
+
+export const Basic = Template.bind({});
+
+export const WithIcon = Template.bind({});
+
+WithIcon.args = {
+  ...Basic.args,
+  icon: <Add />,
 };
 
-export const Basic = (): JSX.Element => (
-  <BackGround>
-    <Button title="Submit" />
-  </BackGround>
-);
+export const WithIconAndTextLeft = Template.bind({});
 
-export const WithIcon = (): JSX.Element => (
-  <BackGround>
-    <Button title="Submit" icon={<Add />} />
-  </BackGround>
-);
-
-export const WithIconAndTextLeft = (): JSX.Element => (
-  <BackGround>
-    <Button title="Submit" icon={<Add />} textLeft />
-  </BackGround>
-);
+WithIconAndTextLeft.args = {
+  ...Basic.args,
+  textLeft: true,
+  icon: <Add />,
+};
 
 const BackGround = styled.div`
   width: 100%;
