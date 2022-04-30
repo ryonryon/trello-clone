bbdb6d82a709d0351e112ad6384b0eff74a4f1a2
Ryo Togashi <ryo.togashi.ca@gmail.com>
2022-04-30 16:42:29 -0700

    Add EditableLabel

---
+++ src/components/EditableLabel/EditableLabel.tsx
@@ -0,0 +1,51 @@
+import { CSSProperties, FormEvent, KeyboardEvent, useRef, useState } from "react";
+import styled from "styled-components";
+
+interface RootBodyProps {
+  value?: string;
+  onBlur?: (value: string) => void;
+  className?: string;
+  style?: CSSProperties;
+}
+
+export interface Props extends RootBodyProps {}
+
+export default function EditableLabel({ value: initValue = "", onBlur = () => {} }: Props): JSX.Element {
+  const [value, setValue] = useState<string>(initValue);
+  const ref = useRef<HTMLInputElement>(null);
+
+  const handleChange = (e: FormEvent<HTMLInputElement>) => {
+    setValue(e.currentTarget.value);
+  };
+
+  const handleBlur = () => {
+    onBlur(value);
+  };
+
+  const handleKeyDown = ({ key }: KeyboardEvent) => {
+    if (key === "Enter" || key === "Escape") {
+      ref.current?.blur();
+
+      onBlur(value);
+    }
+  };
+
+  return (
+    <Root
+      aria-label="editable-label"
+      ref={ref}
+      value={value}
+      onChange={handleChange}
+      onBlur={handleBlur}
+      onKeyDown={handleKeyDown}
+    />
+  );
+}
+
+const Root = styled.input`
+  border: none;
+  background-color: inherit;
+  padding-left: 4px;
+  min-height: 24px;
+  font-size: 16px;
+`;
