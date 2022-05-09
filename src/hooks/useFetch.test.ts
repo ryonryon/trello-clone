a9db2a29f71b7490a567c3498f864255d1919dbc
kei95 <kei0613shine.star@gmail.com>
2022-05-08 19:39:53 -0700

    Add useFetch hooks to the project

---
+++ src/hooks/useFetch.test.ts
@@ -0,0 +1,79 @@
+import { renderHook, waitFor, act } from "@testing-library/react";
+import { useFetch } from "./useFetch";
+
+describe("useFetch()", () => {
+  test("Passed valid url - should return expected result body", async () => {
+    // arrange
+    let result: { current: ReturnType<typeof useFetch> };
+    const mockedUrl = "https://www.google.com";
+    const returnBody = {
+      status: 200,
+      body: {
+        title: "Yay mocked google title",
+      },
+    };
+    const expectedReturnBody = returnBody.body;
+
+    // Mock fetch api with successful response body
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        json: () => Promise.resolve(JSON.stringify(returnBody.body)),
+        ...returnBody,
+      }),
+    ) as jest.Mock;
+
+    // act
+    await act(async () => {
+      result = renderHook(() => useFetch(mockedUrl)).result;
+    });
+
+    // assert
+    await waitFor(() => expect(result.current.error).toEqual(null));
+    await waitFor(() => expect(result.current.data).toEqual(expectedReturnBody));
+    await waitFor(() => expect(result.current.isLoading).toEqual(false));
+  });
+
+  test("Passed valid url BUT returned error - should return expected error body", async () => {
+    // arrange
+    let result: { current: ReturnType<typeof useFetch> };
+    const mockedUrl = "https://www.google.com";
+    const expectedErrorBody = {
+      status: 500,
+      statusText: "internal error",
+    };
+
+    // Mock fetch api with error response body
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        json: () => Promise.resolve(JSON.stringify(expectedErrorBody)),
+        ...expectedErrorBody,
+      }),
+    ) as jest.Mock;
+
+    // act
+    await act(async () => {
+      result = renderHook(() => useFetch(mockedUrl)).result;
+    });
+
+    // assert
+    await waitFor(() => expect(result.current.error).toEqual(expectedErrorBody));
+    await waitFor(() => expect(result.current.data).toEqual(null));
+    await waitFor(() => expect(result.current.isLoading).toEqual(false));
+  });
+
+  test("Passed NO url - shouldn't do anything, so returns default values", async () => {
+    // arrange
+    let result: { current: ReturnType<typeof useFetch> };
+    const mockedUrl = "";
+
+    // act
+    await act(async () => {
+      result = renderHook(() => useFetch(mockedUrl)).result;
+    });
+
+    // assert
+    await waitFor(() => expect(result.current.error).toEqual(null));
+    await waitFor(() => expect(result.current.data).toEqual(null));
+    await waitFor(() => expect(result.current.isLoading).toEqual(false));
+  });
+});
---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+
+interface ErrorBody {
+  status: number;
+  statusText: string;
+}
+
+export function useFetch<TResponse>(
+  url: string,
+  requestBody?: RequestInit,
+): { data: null | TResponse; isLoading: boolean; error: null | unknown } {
+  const [data, setData] = useState<null | TResponse>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<ErrorBody | null>(null);
+
+  useEffect(() => {
+    async function makeRequest(): Promise<void> {
+      setIsLoading(true);
+
+      const response: Response = await fetch(url, requestBody);
+
+      // Set an error if the request ins't successful
+      if (response.status < 200 || response.status > 299) {
+        setError({ status: response.status, statusText: response.statusText });
+        setIsLoading(false);
+        return;
+      }
+
+      const jsonResponseBody: string = await response.json();
+      const parsedBody: TResponse = JSON.parse(jsonResponseBody);
+
+      setData(parsedBody);
+      setIsLoading(false);
+    }
+
+    url && makeRequest();
+  }, [url, requestBody]);
+
+  return { data, isLoading, error } as const;
+}
